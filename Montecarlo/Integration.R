c6508d83caa0b9df6b11f2b6094087d5bd2a871e
Thomas Mortensen <39585003+Manatee1@users.noreply.github.com>
2018-10-23 12:05:18 +0200

    commit

--- Montecarlo/Integration.R
+++ Montecarlo/Integration.R
@@ -1,14 +1,19 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-pacman::p_load(cubature,emdbook)
+pacman::p_load(cubature,emdbook,mvtnorm)
+
 
 f = function(x){
-  return(dmvnorm(x,mu = rep(0,3),Sigma = diag(3)))
+  return(emdbook::dmvnorm(x,mu = rep(0,3),Sigma = diag(3)))
 }
 g = function(x){
-  return(x)
+  return(sqrt((3-2)/3)*mvtnorm::dmvt(x,delta = rep(0,3),sigma = diag(3),log = F,df=3))
 }
-adaptIntegrate(f,lowerLimit = c(-10,-10,-10),upperLimit = c(-1,-1,-1),absError = 1e-7)
-adaptIntegrate(f,lowerLimit = c(-3,-3,-3),upperLimit = c(-1,-1,-1),absError = 1e-7)
+g2 = function(x){
+  return(mvtnorm::dmvt(x,delta = rep(0,3),sigma = diag(3),log = F,df=3))
+}
+adaptIntegrate(f,lowerLimit = c(-10,-10,-10),upperLimit = rep(10,3),absError = 1e-7)
+adaptIntegrate(g,lowerLimit = rep(-10,3),upperLimit = rep(10,3),absError = 1e-7)
+adaptIntegrate(g2,lowerLimit = rep(-10,3),upperLimit = rep(10,3),absError = 1e-7)
 
 
 
@@ -18,5 +23,6 @@ adaptIntegrate(g,lowerLimit = c(-1,-1,-1),upperLimit = c(1,1,1))
 
 
 
+p = 1:421341841196
 
 
--- Montecarlo/Monte_Carlo_Power_vector.R
+++ Montecarlo/Monte_Carlo_Power_vector.R
@@ -1,6 +1,8 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 pacman::p_load(cubature,emdbook,MASS,mvtnorm,tictoc)
 
+
+
 MC_power = function(c,dist = "norm",B=1e4,dim = 3,rs,alpha = 0.05,
                     inf = 10,tol = 1e-6,df = 3){
   #browser()
@@ -28,63 +30,61 @@ MC_power = function(c,dist = "norm",B=1e4,dim = 3,rs,alpha = 0.05,
                                     upperLimit = r,absError = tol)$integral
     n = c/int1
     for(i in 1:B){
-      WLR_bar = 0
-      hacsigma_cl = 0
-      hacsigma_csl = 0
-      if(i %% 100 == 0){
+      if(i %% 500 == 0){
         print(i)
       }
+      browser()
+      sim = mvrnorm(n, mu = rep(0,3) , Sigma = diag(3))
       
-      j <- 0
-      while(j < n){
-        sim = mvrnorm(1, mu = rep(0,3) , Sigma = diag(3))
-        {#CL
-          Indy <- FUN(sim)
-          S1 = Indy*(log(f(sim)/int1))
-          S2 = Indy*(log(g(sim)/int2))
-          WLR <- S1-S2
-          WLR_bar_cl <- WLR_bar+WLR
-          hacsigma_cl <- hacsigma_cl + WLR^2
-        }
-        {#CSL
-          Indy_c <- FUN_c(sim)
-          S1 = Indy*(log(f(sim))) + Indy_c*(log(1-int1))
-          S2 = Indy*(log(g(sim))) + Indy_c*(log(1-int2))
-          WLR <- S1-S2
-          WLR_bar_csl <- WLR_bar+WLR
-          hacsigma_csl <- hacsigma_csl + WLR^2
-        }
-        j = j + 1
-      }
-      
+      {#CL
+      Indy <- apply(sim,MARGIN = 1,FUN)
+      S1 = Indy*(log(f(sim)/int1))
+      S2 = Indy*(log(g(sim)/int2))
       #browser()
-      hacsigma_cl <- sqrt( hacsigma_cl/n )
-      WLR_bar_cl <- WLR_bar_cl/n
-      t <- WLR_bar_cl*sqrt(n)/(hacsigma_cl)
+      WLR <- S1 - S2
+      WLR.bar <- sum(WLR)/n
+      hacsigma <- sqrt( sum(WLR^2)/n )
+      
+      t <- WLR.bar*sqrt(n)/(hacsigma)
       p <- pnorm(t)
       best_cl = "Not significally different"
-      if(!is.na(p)){
+      if(is.na(p)){
+        best_cl = "Not significally different"
+      }
+      else{
         if(p<alpha/2){
           best_cl = "Density 2"
         }
         if(p>1-alpha/2){
           best_cl = "Density 1"
         }
-      }
+      }}
       
-      hacsigma_csl <- sqrt( hacsigma_csl/n )
-      WLR_bar_csl <- WLR_bar_csl/n
-      t <- WLR_bar_csl*sqrt(n)/(hacsigma_csl)
+      {#CSL
+      Indy_c <- apply(sim,MARGIN = 1,FUN_c)
+      S1 = Indy*(log(f(sim))) + Indy_c*(log(1-int1))
+      S2 = Indy*(log(g(sim))) + Indy_c*(log(1-int2))
+      
+      
+      WLR <- S1 - S2
+      WLR.bar <- sum(WLR)/n
+      hacsigma <- sqrt( sum(WLR^2)/n )
+      
+      t <- WLR.bar*sqrt(n)/(hacsigma)
       p <- pnorm(t)
       best_csl = "Not significally different"
-      if(!is.na(p)){
+      if(is.na(p)){
+        best_csl = "Not significally different"
+      }
+      else{
         if(p<alpha/2){
           best_csl = "Density 2"
         }
         if(p>1-alpha/2){
           best_csl = "Density 1"
         }
-      }
+      }}
+      
       
       Reject_r_count_cl[i,1] = ifelse(best_cl == "Density 1" , 1 , 0)
       Reject_r_count_csl[i,1] = ifelse(best_csl == "Density 1" , 1 , 0)
@@ -102,9 +102,10 @@ MC_power = function(c,dist = "norm",B=1e4,dim = 3,rs,alpha = 0.05,
 rr = 1
 rs = seq(from = -rr, to = rr,by = 0.1)
 set.seed(1)
-tic() ; h = MC_power(c = 5,B = 10000,rs = 0,inf = 10); toc()  
+tic() ; h = MC_power(c = 5,B = 100,rs = -2,inf = 10); toc()  
 tic() ; h = MC_power(c=5,B = 10000,rs = rs,inf = 10); toc()  
 
 save(h,file = "Power.Rdata")
+
 
 
