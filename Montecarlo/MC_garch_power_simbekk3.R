e1543d51109c1f811f2489ffeb2ced17e696e4c9
Thomas Mortensen <39585003+Manatee1@users.noreply.github.com>
2018-11-12 10:04:36 +0100

    sd

--- Montecarlo/MC_garch_power_simbekk3.R
+++ Montecarlo/MC_garch_power_simbekk3.R
@@ -1,7 +1,7 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-pacman::p_load(cubature,emdbook,MASS,mvtnorm,tictoc,parallel,mgarchBEKK,tidyverse,rugarch)
+pacman::p_load(cubature,emdbook,MASS,mvtnorm,tictoc,parallel,mgarchBEKK,tidyverse,rugarch,
+               matrixcalc)
 source("./DATA/DataAndReturnFct.R")
-cl = makePSOCKcluster(10)
 
 
 Rolling_BEKK = function(IS , OS , Spec = c(1,1),dim = 3,rs=c(1)){
@@ -46,40 +46,6 @@ Rolling_BEKK = function(IS , OS , Spec = c(1,1),dim = 3,rs=c(1)){
   return(OneSigma)
   
 }
-RollingForecast = function(IS , OS ){
-  Spec = ugarchspec(variance.model = list( model = "sGARCH", garchOrder = c(1,1)),
-                    mean.model = list( armaOrder = c(0,0) , include.mean = F) )
-  All_Data = c( IS, OS)
-  
-  n = length(IS)
-  m = length(OS)
-  
-  OneSigma = rep(0,m)
-  
-  for (i in 1:m) {
-    Current_Data = All_Data[i:(n-1+i)]
-    if(i %% 5 == 0 | i == 1){
-      Fit <- ugarchfit(Spec, data = Current_Data , solver = "hybrid",cluster = cl)
-      omega <- Fit@fit$coef[1]
-      alpha <- Fit@fit$coef[2]
-      beta <- Fit@fit$coef[3]
-      H <- tail(Fit@fit$sigma,1)^2
-    }
-    else{
-      H <- omega + alpha*res^2 + beta*H
-    }
-    
-    res = Current_Data[n]
-    
-    H = omega + alpha*res^2 + beta*H
-    
-    OneSigma[i] = H %>% as.numeric()
-    
-  }
-  
-  return(OneSigma)
-  
-}
 
 
 MC_power_Bekk <- function(in.sample,out.sample,alpha = 0.05,B = 100){
@@ -92,13 +58,12 @@ MC_power_Bekk <- function(in.sample,out.sample,alpha = 0.05,B = 100){
     Reject_r_count_csl <- matrix(data = 0, nrow = B , ncol = 2)
     All_data = rbind(in.sample,out.sample)
     
-    Fit <- BEKK(All_data)
-    H_list <- Fit$H.estimated
-    Parameters = c(vech(t(Fit$est.params[[1]])))
-    for(i in 2:length(Fit$est.params)){
-      Parameters = c(Parameters,vec(Fit$est.params[[i]]))
+
+    for(j in 1:3){
+      assign(paste("Fit",j,sep = "_"),
+             ugarchfit(spec = Spec,data = All_data[,j],solver = "hybrid"))
     }
-    
+    fits <- list(Fit_1,Fit_2,Fit_3) 
     
     int1 = 1
     int2 = 1
@@ -115,37 +80,40 @@ MC_power_Bekk <- function(in.sample,out.sample,alpha = 0.05,B = 100){
   
   #sim <- list()
   for(i in 1:B){
-    simbekk <- simulateBEKK(3,is+os,params = Parameters)
-    sim <- matrix(0,nrow = is+os,ncol = 3)
-    for(j in 1:3){
-      sim[,j] <- simbekk$eps[[j]]
-    }
-    H_f <- Rolling_BEKK(IS = sim[1:is,],OS = sim[(is+1):(is+os),])
-    #g_matrix <- matrix(0,ncol = 3,nrow = os)
-    #for(j in 1:3){
-    #  g_matrix[,j] <- RollingForecast(IS = sim[1:is,j],OS = sim[(is+1):(is+os),j])
-    #}
     
-    #Spec = ugarchspec(variance.model = list( model = "sGARCH", garchOrder = c(1,1)),
-    #                  mean.model = list( armaOrder = c(0,0) , include.mean = F) )
-    #for(j in 1:3){
-    #  g_matrix[,j] <- ugarchroll(spec = Spec,data = All_data[,j],forecast.length = os,
-    #                             refit.every = 10,refit.window = "moving",solver = "hybrid",
-    #                             calculate.VaR = F,window.size = is)
-    #}
+    for(j in 1:3){
+      assign(paste("simG",j,sep = "_"),
+             ugarchsim(fit = fits[[1]],n.sim = length(All_data[,1])))
+    }
+
+    sim <- matrix(0,nrow = is+os,ncol = 3)
+    sim[,1] <- simG_1@simulation$seriesSim
+    sim[,2] <- simG_2@simulation$seriesSim
+    sim[,3] <- simG_3@simulation$seriesSim
+    
+    H_f <- Rolling_BEKK(IS = sim[1:is,],OS = sim[(is+1):(is+os),])
+    g_matrix <- matrix(0,ncol = 3,nrow = os)
+
+    Spec = ugarchspec(variance.model = list( model = "sGARCH", garchOrder = c(1,1)),
+                      mean.model = list( armaOrder = c(0,0) , include.mean = F) )
+    
+    for(j in 1:3){
+      roll = ugarchroll(spec = Spec,data = sim[,j],forecast.length = os,
+                        refit.every = 5,refit.window = "moving",solver = "hybrid",
+                        calculate.VaR = F,window.size = is)
+      g_matrix[,j] <- (roll@forecast$density$Sigma)^2
+    }
+    
     H_g = list()
-    #for(j in 1:os){
-    #  H_g[[j]] <- diag(g_matrix[j,])
-    #}
     for(j in 1:os){
-      H_g[[j]] <- cov(All_data[j:(j+is),])
+      H_g[[j]] <- diag(g_matrix[j,])
     }
     
     #CL
     {
       Indy <- 1
-      S1 <- Indy*(log(f(sim[(is+1):(is+os),],H_g)/int1))
-      S2 <- Indy*(log(f(sim[(is+1):(is+os),],H_f)/int2))
+      S1 <- Indy*(log(f(sim[(is+1):(is+os),],H_f)/int1))
+      S2 <- Indy*(log(f(sim[(is+1):(is+os),],H_g)/int2))
       
       #browser()
       
@@ -167,32 +135,7 @@ MC_power_Bekk <- function(in.sample,out.sample,alpha = 0.05,B = 100){
           best_cl <- "Density 1"
         }
       }}
-    #CSL
-    {
-      #   Indy_c <- 0
-      #   S1 <- Indy*(log(f(sim))) + Indy_c*(log(1-int1))
-      #   S2 <- Indy*(log(g(sim))) + Indy_c*(log(1-int2))
-      # 
-      # 
-      #   WLR <- S1 - S2
-      #   WLR.bar <- sum(WLR)/n
-      #   hacsigma <- sqrt( sum(WLR^2)/n )
-      # 
-      #   t <- WLR.bar*sqrt(n)/(hacsigma)
-      #   p <- pnorm(t)
-      #   best_csl <- "Not significally different"
-      #   if(is.na(p)){
-      #     best_csl <- "Not significally different"
-      #   }
-      #   else{
-      #     if(p<alpha/2){
-      #       best_csl <- "Density 2"
-      #     }
-      #     if(p>1-alpha/2){
-      #       best_csl <- "Density 1"
-      #     }
-      #   }
-    }
+
     
     Reject_r_count_cl[i,1]<-ifelse(best_cl == "Density 1" , 1 , 0)
     #Reject_r_count_csl[i,1]<-ifelse(best_csl == "Density 1" , 1 , 0)
@@ -215,7 +158,7 @@ set.seed(1)
 tic() ; Result = MC_power_Bekk(in.sample = DF[(end-100):end,],
                                out.sample = OS[1:19,],B = 10); toc()
 
-save(Result,file = "Garch_power_bekkvCov.Rdata")
+save(Result,file = "Garch_power_GvBekk.Rdata")
 
 
 
