5ecf5996d47468ecb1f111ef549c84bb5da7a5f3
Thomas Mortensen <STUDENT\tmort15@math-js04.srv.aau.dk>
2018-11-29 08:33:59 +0100

    d

--- Bekk_Garch.R
+++ Bekk_Garch.R
@@ -6,10 +6,6 @@ source("Rolling_BEKK.R")
 load("./DATA/Workspace2006.RData")
 
 
-DF = Return_DF[,5:7] %>% as.data.frame() %>% as.matrix(); 
-DF2 = Return_DF_OOS[,5:7] %>% as.data.frame() %>% as.matrix() ; 
-Data <- rbind(DF,DF2);end = length(Data[,1]); OS <- Data[(1510):end,]; IS <- Data[1:1509,]
-os <- length(OS[,1]) ; is <- length(IS[,1])
 
 
 
